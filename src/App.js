e2b030070a8952a38d16c3e05184a140372a52c5
Jorge Esparza <corinthian@Jorges-Air.lan>
2021-01-05 22:52:12 -0500

    Created Generator Class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import Generator from './Main'
 import './App.css';
+im
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      </div>
+
+    </div>
   );
 }
 
---
+++ src/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import Axios from "axios"
+
+ class Generator extends React.Component {
+     constructor(props) {
+         super(props);
+         this.state = {
+             setup: "",
+             punchLine: ""
+         }
+     }
+
+     render(){
+         return (
+             <div>
+
+             </div>
+
+         )
+     }
+ }
+
+ export default Generator
